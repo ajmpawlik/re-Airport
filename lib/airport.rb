a69154128d8a2564e224b33dcd9f80771a8317d0
Aleksandra <ajmpawlik@gmail.com>
2019-08-10 22:33:28 +0100

    FEATURE: creates class Plane, tests for existence of hangar

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,2 +1,16 @@
 class Airport
+
+attr_reader :hangar
+
+  def initialize
+    @hangar = []
+  end
+
+  def land
+    true
+  end
+
+  def get_hangar
+    @hangar
+  end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,14 @@
 require './lib/airport'
 
 describe Airport do
+  it 'responds to method land' do
+    expect(subject).to respond_to :land
+  end
+  it 'lands a plane' do
+    expect(subject.land).to eq true
+  end
+  it 'stores planes' do
+    airport = Airport.new
+    expect(subject.get_hangar).to eq []
+  end
 end
